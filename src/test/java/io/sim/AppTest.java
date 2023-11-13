4c6ae140d808ef5c255680b341e6e7c231dccde3
cleberjunior1 <cleber.junior1@estudante.ufla.br>
2023-11-13 01:17:37 -0300

    junit

--- src/test/java/io/sim/AppTest.java
+++ src/test/java/io/sim/AppTest.java
@@ -1,25 +1,5 @@
 package io.sim;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
-package io.sim;
-
 import org.junit.Test;
 
 import static org.junit.Assert.*;
